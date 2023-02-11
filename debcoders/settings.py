cb12402c4fde42052c13844ade2e71284fb586a7
DebadritoCG <techgamerislive38@gmail.com>
2023-02-11 22:54:58 +0530

    initial

--- debcoders/settings.py
+++ debcoders/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,9 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-]
+)
+
+STATIC_ROOT = "static_files/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
